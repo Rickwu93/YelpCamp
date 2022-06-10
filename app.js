819d34c4dfb9efc77de17554029a77c7b9dc760c
Ricky <rickywu209@gmail.com>
2022-06-09 22:37:15 -0600

    campground index basic page to show all campgrounds in our /campgrounds

--- app.js
+++ app.js
@@ -3,37 +3,34 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', (err) => {
-  
-  if (err) {
-    console.log(err)
-  } else {
-  console.log('connected')
-} 
-}
-);
+mongoose.connect('mongodb://localhost:27017/yelp-camp', err => {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log('connected');
+	}
+});
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('Database connected');
 });
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/makecampground', async (req, res) => {
-  const camp = new Campground({ title: 'My Backyard', description: 'cheap camping' });
-  await camp.save();
-  res.send(camp)
-  // console.log(camp)
-})
+	res.render('home');
+});
+app.get('/campgrounds', async (req, res) => {
+	const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds })
+});
+
 
 app.listen(3000, () => {
-  console.log('Serving on port 3000')
-})
+	console.log('Serving on port 3000');
+});
