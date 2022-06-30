e808b929578bc4892dce180aafefb167409f60ef
Ricky <rickywu209@gmail.com>
2022-06-30 01:05:16 -0600

    creating our first review with a new POST route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', err => {
 	if (err) {
@@ -84,6 +85,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 	await Campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
 }));
+//creating new reviews, we then push the reviews into the campground array
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+	const campground = await Campground.findById(req.params.id);
+	const review = new Review(req.body.review);
+	campground.reviews.push(review);
+	await review.save();
+	await campground.save();
+	res.redirect(`/campgrounds/${campground._id}`);
+}))
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
