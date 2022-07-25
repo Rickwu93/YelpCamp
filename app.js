8ecfcaa9dc04a1fcac97737df17eeeda8128e619
Ricky <rickywu209@gmail.com>
2022-07-25 02:32:02 -0600

    conditional rendering for navbar for signed in users and logged out

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ passport.deserializeUser(User.deserializeUser());
 
 //middleware for before every route handlers and have access to it over local and success
 app.use((req, res, next) => {
+	res.locals.currentUser = req.user;
 	res.locals.success = req.flash('success');
 	res.locals.error = req.flash('error');
 	next();
