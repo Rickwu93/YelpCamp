6e8c35ffa0d00cabbd65aa3b5d86b53bcb8803fa
Ricky <rickywu209@gmail.com>
2022-07-25 02:44:10 -0600

    allow users to stay logged in after registering

--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,17 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 //post route for registration form, we grab the three things, and pass in 2 things for user, then store and hashes
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
   try {
   const {email, username, password} = req.body;
   const user = new User({email, username });
   const registeredUser = await User.register(user, password);
-  req.flash('success','Welcome to Yelp Camp!');
-  res.redirect('/campgrounds');
+  //keeps the user signed in and error handles
+  req.login(registeredUser, err => {
+    if(err) return next(err);
+    req.flash('success','Welcome to Yelp Camp!');
+    res.redirect('/campgrounds');
+      })
   } catch (e) {
       req.flash('error', e.message);
       res.redirect('register')
