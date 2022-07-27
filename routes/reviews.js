fe3951500e828f25483d45a09f240b75f0ab0b90
Ricky <rickywu209@gmail.com>
2022-07-26 22:30:33 -0600

    added user authorization for reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router( {mergeParams: true });
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
@@ -10,9 +10,10 @@ const catchAsync = require('../utils/catchAsync');
 
 
 //creating new reviews, we then push the reviews into the campground array
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	const review = new Review(req.body.review);
+	review.author = req.user._id;
 	campground.reviews.push(review);
 	await review.save();
 	await campground.save();
@@ -20,7 +21,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
 	res.redirect(`/campgrounds/${campground._id}`);
 }))
 //deleting individual reviews, we use pull to pull anything out with that reviewId in reviews
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
 	const { id, reviewId } = req.params;
 	await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
 	await Review.findByIdAndDelete(reviewId);
