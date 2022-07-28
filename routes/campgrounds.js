09d69f42d9e6744e151d2287a438717d721e1d8d
Ricky <rickywu209@gmail.com>
2022-07-27 22:25:47 -0600

    restructuring user and campground routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,19 +6,21 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 const Campground = require('../models/campground');
 
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-router.get('/', catchAsync(campgrounds.index));
-//tells the user in campground/new if they're not logged in that they must signin and will redirect to /login
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+
+router.route('/:id') 
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+    
+
 //post request for creating new campground that would save and redirect back to the campground id
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get('/:id', catchAsync(campgrounds.showCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-//delete individual campgrounds
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,14 +5,16 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+  .get(users.renderRegister)
+  .post(catchAsync(users.register));
 //post route for registration form, we grab the three things, and pass in 2 things for user, then store and hashes
-router.post('/register', catchAsync(users.register));
 
-router.get('/login', users.renderLogin);
+router.route('/login')
+  .get(users.renderLogin)
+  .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 //password authenticate using local strategy, redirect to login if there's a failure redirect or flash failure message if there is
 //redirect user back to original session or to /campgrounds
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
 router.get('/logout', users.logout);
   
