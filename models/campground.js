b242a890aef3f24c127f41ba6a8090d06a67251f
Ricky <rickywu209@gmail.com>
2022-08-03 17:12:56 -0600

    adding custom popups with campground name and description that redirects to campground page

--- models/campground.js
+++ models/campground.js
@@ -11,7 +11,10 @@ const ImageSchema = new Schema({
 
 ImageSchema.virtual('thumbnail').get(function() {
 	return this.url.replace('/upload', '/upload/w_200');
-})
+});
+
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
 	title: String,
   images: [ImageSchema],
@@ -39,7 +42,13 @@ const CampgroundSchema = new Schema({
 			ref: 'Review'
 		}
 	]
-});
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function() {
+	return `
+	<strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+	<p>${this.description.substring(0, 20)}...</p>`
+})
 
 //this doc has reviews we're basically deleting all reviews where their ID field is in our document that was just deleted in its review array
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
